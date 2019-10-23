a2e96d41ca8f0db844bf5c0fa2b7073e133e80a4
= <zainr7989@gmail.com>
2019-10-22 17:45:25 -0700

    [ADD] required dependencies text file

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/zainraza/Documents/dev/courses/learn-django-live/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
