5a485ebce2811d6dbcd1de3a6ce0abdbec96e962
= <zainr7989@gmail.com>
2019-10-25 09:27:02 -0700

    [FIX} Mispelling in Question model definition.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
