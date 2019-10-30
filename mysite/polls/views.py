2f0550ecaacc366167b9f29ecd3e27fa076b5713
= <zainr7989@gmail.com>
2019-10-29 17:52:10 -0700

    [ADD] Views for the polls app.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,4 +5,21 @@ from django.http import HttpResponse
 
 
 def index(request):
+    """A view for the index page."""
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def detail(request, question_id):
+    """A view for the question being asked."""
+    return HttpResponse(f"You're looking at question {question_id}")
+
+
+def results(request, question_id):
+    """A view to see answers so far on the poll."""
+    response = f"You're looking at the results for {question_id}"
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    """A view for the voting page."""
+    return HttpResponse(f"You're voting on question {question_id}")
