fe642d575274219b94c714dce2b68793fb55baf6
= <zainr7989@gmail.com>
2019-10-31 09:18:03 -0700

    [UPDATE] Index view to use updated index template.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
-
-# Create your views here.
+from django.template import loader
 
 
 def index(request):
     """Display latest 5 questions."""
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
