60495029f8ae764296c663f2ee763bcedaeca27e
= <zainr7989@gmail.com>
2019-10-31 11:54:00 -0700

    [UPDATE] Implement vote view to use server data.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from .models import Question, Choice
+from django.urls import reverse
 
 
 def index(request):
@@ -25,4 +25,20 @@ def results(request, question_id):
 
 def vote(request, question_id):
     """A view for the voting page."""
-    return HttpResponse(f"You're voting on question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request,
+                      "polls/detail.html",
+                      {
+                        'question': question,
+                        'error_message': "You didn't select a choice.",
+                      })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # prevent data from being posted twice in case hits Back button
+        return HttpResponseRedirect(reverse('polls:results',
+                                            args=(question.id,)))
