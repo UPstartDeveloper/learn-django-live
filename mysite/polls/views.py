56b6d1dbc6abb390940088771a4657d1edba02cf
= <zainr7989@gmail.com>
2019-10-31 12:34:10 -0700

    [UPDATE] Amend views using generic template system in Django.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,26 +1,30 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from .models import Choice, Question
 from django.urls import reverse
+from django.views import generic
 
 
-def index(request):
-    """Display latest 5 questions."""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+class IndexView(generic.ListView):
+    """Index view implemented using generic templating system."""
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    """A view for the question being asked."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    """View for one question implemented using generic templating system."""
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(request, question_id):
-    """A view to see answers so far on the poll."""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    """Results view implemented using generic templating system."""
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
