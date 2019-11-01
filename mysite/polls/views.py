ea54a92fdae634b84a62eea7238f65e12f2a2a2c
= <zainr7989@gmail.com>
2019-10-31 18:02:01 -0700

    [UPDATE] IndexView to exclude questions published in the future.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Choice, Question
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -11,8 +12,11 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions.
+           Does not include those to be published in the future.
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
