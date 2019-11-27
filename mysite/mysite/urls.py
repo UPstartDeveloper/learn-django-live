8fdfdbdc8d5ffc85e1a5173466106667284210ef
= <zainr7989@gmail.com>
2019-11-26 16:04:20 -0800

    [ADD] URL patterns for API app.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import api
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
