99c84566d29fa70d8b8a94fec46ca3eeaa673e72
= <zainr7989@gmail.com>
2019-11-26 17:48:57 -0800

    [ADD] Rest framework urls.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, url
 import api
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
