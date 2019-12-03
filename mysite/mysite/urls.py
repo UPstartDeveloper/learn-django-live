c6a7c6328969fc74693788a39332864c88e2152e
= <zainr7989@gmail.com>
2019-12-03 15:39:34 -0800

    [ADD] STATICFILES_DIRS setting.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, url
-import api
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
